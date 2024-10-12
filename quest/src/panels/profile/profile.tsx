bd50e81efad232ffb1fe0ead3d046374d3fba838
Shahnavaz Gasanov <32392840+Shakhnavaz@users.noreply.github.com>
2024-10-12 03:38:56 +0300

    profile layout

--- quest/src/panels/profile/profile.tsx
+++ quest/src/panels/profile/profile.tsx
@@ -1,11 +1,36 @@
-import { Panel, PanelProps } from "@vkontakte/vkui";
+import { Panel, PanelProps, Avatar, Flex,  PanelHeader, FixedLayout, Gradient, Button, Placeholder } from "@vkontakte/vkui";
 
 export const ProfilePanel = (props: PanelProps) => {
     const { ...rest } = props;
 
     return (
         <Panel {...rest} disableBackground>
-            <h1>Profile</h1>
-        </Panel>
-    );
-};
+
+              <PanelHeader>Profile</PanelHeader>
+              <FixedLayout vertical="bottom">
+                <Gradient to="top">
+                  <Flex justify="center" style={{ padding: 32 }}>
+                    <Button>Click</Button>
+                  </Flex>
+                </Gradient>
+              </FixedLayout>
+              
+                <Gradient mode="tint" to="top">
+                  <Placeholder
+                    icon={<Avatar size={96} />}
+                    header="Гасанов Шахнаваз"
+                    action={
+                      <Button size="m" mode="secondary">
+                        Редактировать
+                      </Button>
+                    }
+                  >
+                    Студент
+                  </Placeholder>
+                </Gradient>
+                  
+                  
+            </Panel>
+        );
+      };
+      
