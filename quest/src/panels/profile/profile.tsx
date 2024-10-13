3f9288da0efacbbb0a76a3a4a3f165f841ff51f5
Shahnavaz Gasanov <32392840+Shakhnavaz@users.noreply.github.com>
2024-10-13 03:01:55 +0300

    profile layout

--- quest/src/panels/profile/profile.tsx
+++ quest/src/panels/profile/profile.tsx
@@ -1,5 +1,20 @@
-import { Panel, PanelHeader, PanelProps } from "@vkontakte/vkui";
+import {
+    SimpleCell,
+    Header,
+    Image,
+    Panel,
+    PanelHeader,
+    PanelProps,
+    View,
+    Group,
+    Flex,
+    Cell,
+    Avatar,
+    CardGrid,
+    Button,
+} from "@vkontakte/vkui";
 import { useProfileContext } from "src/contexts/profileContext";
+import { Icon20TestCoinOutline } from "@vkontakte/icons";
 
 export const ProfilePanel = (props: PanelProps) => {
     const { ...rest } = props;
@@ -9,7 +24,84 @@ export const ProfilePanel = (props: PanelProps) => {
     return (
         <Panel {...rest} disableBackground>
             <PanelHeader fixed>Profile</PanelHeader>
-            {`Token Amount: ${profile?.tokenAmount}`}
+
+            <View activePanel="list">
+                <Panel id="list">
+                    <Group>
+                        <Flex direction="row" justify="space-between">
+                            <Cell before={<Avatar />} subtitle="Студент">
+                                Иван Итмошников
+                            </Cell>
+                            <Cell>{` ${profile?.tokenAmount} Токенов`}</Cell>
+                        </Flex>
+                    </Group>
+
+                    <Flex justify="center"></Flex>
+
+                    <Flex justify="center"></Flex>
+                    <Group mode="plain">
+                        <Flex justify="center" align="center">
+                            <CardGrid size="l">
+                                <div>
+                                    <Group
+                                        header={
+                                            <Header mode="secondary">
+                                                Достижения
+                                            </Header>
+                                        }
+                                    >
+                                        <SimpleCell
+                                            style={{ padding: 16 }}
+                                            before={
+                                                <Image
+                                                    src={
+                                                        "https://vkma-course-dishes-backend.prod.kapps.vk-apps.com/storage/achivements/8orders.png"
+                                                    }
+                                                />
+                                            }
+                                        >
+                                            Получить первые баллы
+                                        </SimpleCell>
+                                        <SimpleCell
+                                            style={{ padding: 16 }}
+                                            before={
+                                                <Image
+                                                    src={
+                                                        "https://vkma-course-dishes-backend.prod.kapps.vk-apps.com/storage/achivements/5orders.png"
+                                                    }
+                                                />
+                                            }
+                                        >
+                                            Посетить мероприятие 5 раз
+                                        </SimpleCell>
+                                        <SimpleCell
+                                            style={{ padding: 16 }}
+                                            before={
+                                                <Image
+                                                    src={
+                                                        "https://vkma-course-dishes-backend.prod.kapps.vk-apps.com/storage/achivements/first_order.png"
+                                                    }
+                                                />
+                                            }
+                                        >
+                                            Посетить мероприятие 15 раз
+                                        </SimpleCell>
+                                    </Group>
+                                </div>
+                            </CardGrid>
+                        </Flex>
+                    </Group>
+
+                    <Button
+                        size="l"
+                        mode="outline"
+                        align="center"
+                        marginWidth={20}
+                    >
+                        Форма для получения доступа
+                    </Button>
+                </Panel>
+            </View>
         </Panel>
     );
 };
